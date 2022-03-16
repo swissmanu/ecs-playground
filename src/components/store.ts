8195c6cdb343e0b5afcc28e7b0d95e22f7f17f96
Manuel Alabor <manuel@alabor.me>
2022-03-16 22:02:58 +0100

    Basic Logistics System

--- src/components/store.ts
+++ src/components/store.ts
@@ -1,9 +1,9 @@
-import Component from "../entityManager/component";
+import Component from '../entityManager/component';
 
-export default class StoreComponent extends Component<
-  typeof StoreComponent.TypeTag
-> {
-  static TypeTag = "Store" as const;
+export default class StoreComponent extends Component<typeof StoreComponent.TypeTag> {
+  static TypeTag = 'Store' as const;
+
+  public storedItems = 0;
 
   constructor() {
     super(StoreComponent.TypeTag);
--- src/systems/logistics.ts
+++ src/systems/logistics.ts
@@ -1,5 +1,27 @@
-import System from "../system";
+import StoreComponent from '../components/store';
+import System from '../system';
 
 export default class LogisticsSystem extends System {
-  update(): void {}
+  update(): void {
+    for (const [id, components] of this.entityManager.entitiesWithComponent('Store', 'GraphNode')) {
+      if (components.Store.storedItems > 0) {
+        const neighborStores = components.GraphNode.neighbors.reduce<ReadonlyArray<StoreComponent>>(
+          (acc, { entityId }) => {
+            const neighbor = this.entityManager.getEntity(entityId);
+            if (neighbor?.Store) {
+              return [...acc, neighbor.Store!];
+            }
+            return acc;
+          },
+          []
+        );
+
+        if (neighborStores.length > 0) {
+          const [first] = neighborStores;
+          first.storedItems++;
+          components.Store.storedItems--;
+        }
+      }
+    }
+  }
 }
